578fff1438adb01ce8843a38b3ddcfadd005992e
Ram Dettmer <ram@hungryram.com>
2023-08-09 22:18:11 -0700

    update

--- src/app/(site)/components/templates/hero-custom.tsx
+++ src/app/(site)/components/templates/hero-custom.tsx
@@ -77,9 +77,9 @@ export default function HeroCustom({
                                         />
                                     </div>
                                 </div>
-                                <div>
+                                {/* <div>
                                     <Link href="/sign-up" className="primary-button !bg-[#7D0202] !text-white uppercase">Sign up for early access</Link>
-                                </div>
+                                </div> */}
                                 {/* <FormBuilder
                                     formSchema={formBuilder}
                                 /> */}
