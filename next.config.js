b2c7af3be448b1c85763b81e401a8e66c06973ef
Ram Dettmer <ram@hungryram.com>
2026-04-08 09:22:40 -0700

    updates

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
     // assetPrefix: 'https://multipurpose-v2.vercel.app',
+    async rewrites() {
+        return [
+            {
+                source: '/reset-password',
+                destination: '/reset-password.html',
+            },
+            {
+                source: '/stripe-account-linked',
+                destination: '/Stripe_Account_Linked.html',
+            },
+            {
+                source: '/stripe-account-link-expired',
+                destination: '/Stripe_Account_Link_Expired.html',
+            },
+        ]
+    },
     images: {
         remotePatterns: [
             { hostname: 'cdn.sanity.io' },
